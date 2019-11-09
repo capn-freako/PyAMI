bef0b8bcc61a58bf8dfbf4f76aa3abb8515ba0e1
David Banas <david@luminouscomputing.com>
2019-11-09 13:57:21 -0800

    Added I-V curve plotting to Model GUI.

--- pyibisami/ibis_parser.py
+++ pyibisami/ibis_parser.py
@@ -12,10 +12,13 @@ Copyright (c) 2019 by David Banas; All rights reserved World wide.
 """
 
 import re
+import numpy as np
 from parsec       import regex, eof, many1, many, string, generate, sepBy1, \
                          one_of, skip, none_of, times, ParseError, count, separated
 from traits.api   import HasTraits, Trait, String, Float, List
 from traitsui.api import Item, View, ModalButtons, Group, spring
+from chaco.api    import ArrayPlotData, Plot
+from enable.component_editor import ComponentEditor
 
 class IBISModel(HasTraits):
     """
@@ -151,7 +154,8 @@ class Model(HasTraits):
 
         # Fetch available keyword/parameter definitions.
         def maybe(name):
-            return subDict[name] if name in subDict else '(n/a)'
+            # return subDict[name] if name in subDict else '(n/a)'
+            return subDict[name] if name in subDict else None
         self._mtype  = maybe('model_type')
         self._ccomp  = maybe('c_comp')
         self._cref   = maybe('cref')
@@ -160,30 +164,67 @@ class Model(HasTraits):
         self._rref   = maybe('rref')
         self._trange = maybe('temperature_range')
         self._vrange = maybe('voltage_range')
-        self.add_trait('model_type', String(self._mtype))
-        self.add_trait('c_comp', String(self._ccomp))
-        self.add_trait('cref',   String(self._cref))
-        self.add_trait('vref',   String(self._vref))
-        self.add_trait('vmeas',  String(self._vmeas))
-        self.add_trait('rref',   String(self._rref))
-        self.add_trait('trange', String(self._trange))
-        self.add_trait('vrange', String(self._vrange))
-        self._content = [
-            Group(
-                Item('model_type', label='Model type', style='readonly'),
-                Item('c_comp', label='Ccomp', style='readonly'),
-                Item('trange', label='Temperature Range', style='readonly'),
-                Item('vrange', label='Voltage Range', style='readonly'),
-                Group(
-                    Item('cref', label='Cref', style='readonly'),
-                    Item('vref', label='Vref', style='readonly'),
-                    Item('vmeas', label='Vmeas', style='readonly'),
-                    Item('rref', label='Rref', style='readonly'),
-                    orientation="horizontal",
-                ),
-                label='Model', show_border=True,
-            ),
-            ]
+        self._ramp   = maybe('ramp')
+
+        # Infer impedance and rise/fall time.
+        if not self._vrange:
+            raise LookupError("Missing [Voltage Range]!")
+        if 'pulldown' not in subDict or 'pullup' not in subDict:
+            raise LookupError("Missing I-V curves!")
+        plotdata = ArrayPlotData()
+        def proc_iv(xs):
+            if len(xs) < 2:
+                raise ValueError("Insufficient number of I-V data points!")
+            vs, iss = zip(*(xs))  # Idiomatic Python for `unzip`.
+            ityps, imins, imaxs = zip(*iss)
+            vmeas = self._vmeas
+            def calcZ(x):
+                (vs, ivals) = x
+                if vmeas:
+                    ix = np.where(np.array(vs) >= vmeas)[0][0]
+                else:
+                    ix = np.where(np.array(vs) >= max(vs)/2)[0][0]
+                return abs((vs[ix] - vs[ix-1])/(ivals[ix] - ivals[ix-1]))
+            zs = map(calcZ, zip([vs, vs, vs], [ityps, imins, imaxs]))
+            return vs, ityps, imins, imaxs, zs
+        pd_vs, pd_ityps, pd_imins, pd_imaxs, pd_zs = proc_iv(subDict['pulldown'])
+        pu_vs, pu_ityps, pu_imins, pu_imaxs, pu_zs = proc_iv(subDict['pullup'])
+        pu_vs = self._vrange[0] - np.array(pu_vs)  # Correct for Vdd-relative pull-up voltages.
+        pu_ityps = -np.array(pu_ityps)             # Correct for current sense, for nicer plot.
+        pu_imins = -np.array(pu_imins)
+        pu_imaxs = -np.array(pu_imaxs)
+        self._zout = (list(pd_zs)[0] + list(pu_zs)[0])/2
+        plotdata.set_data("pd_vs",    pd_vs)
+        plotdata.set_data("pd_ityps", pd_ityps)
+        plotdata.set_data("pd_imins", pd_imins)
+        plotdata.set_data("pd_imaxs", pd_imaxs)
+        plotdata.set_data("pu_vs",    pu_vs)
+        plotdata.set_data("pu_ityps", pu_ityps)
+        plotdata.set_data("pu_imins", pu_imins)
+        plotdata.set_data("pu_imaxs", pu_imaxs)
+        plot_iv = Plot(plotdata)  # , padding_left=75)
+        # The 'line_style' trait of a LinePlot instance must be 'dash' or 'dot dash' or 'dot' or 'long dash' or 'solid'.
+        plot_iv.plot(("pd_vs", "pd_ityps"), type="line", color="blue", line_style="solid", name="PD-Typ")
+        plot_iv.plot(("pd_vs", "pd_imins"), type="line", color="blue", line_style="dot",   name="PD-Min")
+        plot_iv.plot(("pd_vs", "pd_imaxs"), type="line", color="blue", line_style="dash",  name="PD-Max")
+        plot_iv.plot(("pu_vs", "pu_ityps"), type="line", color="red",  line_style="solid", name="PU-Typ")
+        plot_iv.plot(("pu_vs", "pu_imins"), type="line", color="red",  line_style="dot",   name="PU-Min")
+        plot_iv.plot(("pu_vs", "pu_imaxs"), type="line", color="red",  line_style="dash",  name="PU-Max")
+        plot_iv.title = "Pull-Up/Down I-V Curves"
+        plot_iv.index_axis.title = "Vout (V)"
+        plot_iv.value_axis.title = "Iout (A)"
+        plot_iv.index_range.low_setting  = 0
+        plot_iv.index_range.high_setting = self._vrange[0]
+        plot_iv.value_range.low_setting  = 0
+        plot_iv.value_range.high_setting = 0.1
+        plot_iv.legend.visible = True
+        plot_iv.legend.align = "ul"
+        self.plot_iv = plot_iv
+
+        if not self._ramp:
+            raise LookupError("Missing [Ramp]!")
+        ramp = subDict['ramp']
+        self._slew = (ramp['rising'][0] + ramp['falling'][0])/2e9  # (V/ns)
 
         # Separate AMI executables by OS.
         def is64(x):
@@ -213,6 +254,37 @@ class Model(HasTraits):
             exec64s, exec32s = partition(is64, execs)
             self._exec32Wins, self._exec32Lins = list(map(lambda x: list(map(getFiles, x)), partition(isWin, exec32s)))
             self._exec64Wins, self._exec64Lins = list(map(lambda x: list(map(getFiles, x)), partition(isWin, exec64s)))
+
+        # Set up the GUI.
+        self.add_trait('model_type', String(self._mtype))
+        self.add_trait('c_comp', String(self._ccomp))
+        self.add_trait('cref',   String(self._cref))
+        self.add_trait('vref',   String(self._vref))
+        self.add_trait('vmeas',  String(self._vmeas))
+        self.add_trait('rref',   String(self._rref))
+        self.add_trait('trange', String(self._trange))
+        self.add_trait('vrange', String(self._vrange))
+        self.add_trait('zout',   String(self._zout))
+        self.add_trait('slew',   String(self._slew))
+        self._content = [
+            Group(
+                Item('model_type', label='Model type',            style='readonly'),
+                Item('zout',       label='Impedance (Ohms)',      style='readonly', format_str='%4.1f'),
+                Item('c_comp',     label='Ccomp',                 style='readonly'),
+                Item('trange',     label='Temperature Range',     style='readonly'),
+                Item('vrange',     label='Voltage Range',         style='readonly'),
+                Item('slew',       label='Slew Rate (V/ns)',      style='readonly', format_str='%4.1f'),
+                Group(
+                    Item('cref', label='Cref', style='readonly'),
+                    Item('vref', label='Vref', style='readonly'),
+                    Item('vmeas', label='Vmeas', style='readonly'),
+                    Item('rref', label='Rref', style='readonly'),
+                    orientation="horizontal",
+                ),
+                Item('plot_iv', editor=ComponentEditor(), show_label=False),
+                label='Model', show_border=True,
+            ),
+        ]
 
     def __str__(self):
         res = "Model Type:\t" + self._mtype + '\n'
