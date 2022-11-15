ae207435f2e46c5a63260f358306515ee3bc4571
David Banas <capn.freako@gmail.com>
2022-11-15 07:24:49 -0800

    Minor clean-up, some by linter.

--- src/pyibisami/__init__.py
+++ src/pyibisami/__init__.py
@@ -1,4 +1,5 @@
-"""A package of Python modules, used to configure and test IBIS-AMI models.
+"""
+A package of Python modules, used to configure and test IBIS-AMI models.
 
 .. moduleauthor:: David Banas <capn.freako@gmail.com>
 
@@ -8,7 +9,7 @@ Original Date:   3 July 2012
 
 Copyright (c) 2012 by David Banas; All rights reserved World wide.
 """
-__version__ = "3.5.7"
-__date__ = "September 18, 2022"
-__authors__ = "David Banas"
-__copy__ = "Copyright (c) 2014 David Banas"
+__version__ = "4.0.0"
+__date__    = "November 15, 2022"
+__authors__ = "David Banas & David Patterson"
+__copy__    = "Copyright (c) 2012 David Banas"
