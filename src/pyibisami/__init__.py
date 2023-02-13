a97742a4278171bb74fe8a50e04b9a4b69b47536
David Banas <capn.freako@gmail.com>
2023-02-13 06:40:50 -0500

    Release v4.0.6

--- src/pyibisami/__init__.py
+++ src/pyibisami/__init__.py
@@ -8,7 +8,7 @@ Original Date:   3 July 2012
 
 Copyright (c) 2012 by David Banas; All rights reserved World wide.
 """
-__version__ = "4.0.0"
-__date__ = "November 15, 2022"
+__version__ = "4.0.6"
+__date__ = "February 13, 2023"
 __authors__ = "David Banas & David Patterson"
-__copy__ = "Copyright (c) 2012 David Banas"
+__copy__ = "Copyright (c) 2012 David Banas, 2019 David Patterson"
