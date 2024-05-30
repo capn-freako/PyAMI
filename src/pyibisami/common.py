d0b72f1ce89489813b48b847626255b66c852049
David Banas <capn.freako@gmail.com>
2024-05-30 08:19:12 -0400

    Final additions for Issue 41. (#42)
    * Final additions for Issue 41.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: David Banas <david.banas@keysight.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ src/pyibisami/common.py
@@ -0,0 +1,45 @@
+"""
+Definitions common to all PyIBIS-AMI modules.
+
+Original author: David Banas <capn.freako@gmail.com>
+
+Original date:   May 15, 2024
+
+Copyright (c) 2024 David Banas; all rights reserved World wide.
+"""
+
+from typing import Any, Dict, Iterator, Optional, TypeAlias, TypeVar
+
+import numpy as np  # type: ignore
+import numpy.typing as npt  # type: ignore
+from scipy.linalg import convolution_matrix, lstsq
+
+Real = TypeVar("Real", float, float)
+Comp = TypeVar("Comp", complex, complex)
+Rvec: TypeAlias = npt.NDArray[Real]
+Cvec: TypeAlias = npt.NDArray[Comp]
+
+# PI: float = np.pi  # Causes a failed import of `pyibisami.ami.model` during `tox -e docs`, due to:
+#   File "C:\Users\davibana\prj\PyBERT\PyAMI\src\pyibisami\common.py", line 23, in <module>
+#     TWOPI = 2. * PI
+# TypeError: unsupported operand type(s) for *: 'float' and 'pi'
+# Why?! And what is 'pi'?!
+
+PI: float = 3.141592653589793238462643383279502884
+TWOPI: float = 2.0 * PI
+
+
+def deconv_same(y: Rvec, x: Rvec) -> Rvec:
+    """
+    Deconvolve input from output, to recover filter response, for same length I/O.
+
+    Args:
+        y: output signal
+        x: input signal
+
+    Returns:
+        h: filter impulse response.
+    """
+    A = convolution_matrix(x, len(y), "same")
+    h, _, _, _ = lstsq(A, y)
+    return h
